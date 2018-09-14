3d94fd6f7a46aea6a6bf2a57483b734051cfff12
lhliao <lhliao@g.sce.pccu.edu.tw>
2018-09-14 15:20:15 +0800

    try vue-scroll

--- src/App.vue
+++ src/App.vue
@@ -40,22 +40,11 @@
 
   </b-collapse>
 </b-navbar>
-    <!-- <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
+    <h5>Orders</h5>
+    <ol v-scroll:throttle="{fn: onScroll, throttle: 500 }">
+      <li class="test-scroll" v-for="(msg, index) of msgs" :key="msg">{{msg}} - {{index}}</li>
+    </ol>
+    <div id="ob">scrollTop:{{position.scrollTop}}</div>
   </div>
 </template>
 
@@ -64,8 +53,16 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      msgs : ['Order1', 'Order2', 'Order5', 'Order3', 'Order4', 'Order6', 'Order7', 'Order8', 'Order9', 'Order10', 'Order11', 'Order12', 'Order13'],
+      position: {scrollTop: 0, scrollLeft: 0}
     }
+  },
+  methods:{
+  	onScroll:function(e, position){
+      this.position = position;
+      console.log('1')
+  	}
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuescroll from 'vue-scroll'
 
+Vue.use(vuescroll, {debounce: 600})
 Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  components: { App }
 })
