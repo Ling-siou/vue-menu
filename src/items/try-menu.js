f11da9514f115166e70a391f0c6983006b503180
lhliao <lhliao@g.sce.pccu.edu.tw>
2018-09-17 15:08:34 +0800

    add fake data

---
+++ src/items/try-menu.js
@@ -0,0 +1,29 @@
+var menuItems = [
+    {
+        name: 'Item01',
+        showInMenu: false
+    },{
+        name: 'Item02',
+        showInMenu: false
+    },{
+        name: 'Item03',
+        showInMenu: true
+    },{
+        name: 'Item04',
+        showInMenu: false
+    },{
+        name: 'Item05',
+        showInMenu: false
+    },{
+        name: 'Item06',
+        showInMenu: false
+    },{
+        name: 'Item07',
+        showInMenu: false
+    },{
+        name: 'Item08',
+        showInMenu: false
+    },
+]
+
+module.exports = menuItems
--- src/pages/TryOne.vue
+++ src/pages/TryOne.vue
@@ -1,6 +1,21 @@
 <template>
-    <div></div>
+    <div>
+        <div class="try-one sidebar">
+            <ul>
+                <li v-for="item in menuItems" :key="item.name" v-bind:value="item.name">{{item.name}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 <script>
-    
+
+import menuItems from '../items/try-menu.js'
+    export default {
+        data(){
+            return{
+            menuItems : menuItems
+            }
+        }
+
+    }
 </script>
